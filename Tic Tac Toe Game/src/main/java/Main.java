8fab2a3786a2e0ec28b86d233ffd8869712ac119
Калин Тошев <kalin.toshev84@gmail.com>
2022-08-24 02:53:35 +0300

    Update Main.java

--- Tic Tac Toe Game/src/main/java/Main.java
+++ Tic Tac Toe Game/src/main/java/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         //Using 2D Array of chars to print the board
-        char[][] board = {{' ', ' ', ' '},
-                          {' ', ' ', ' '},
-                          {' ', ' ', ' '}};
+        char[][] board = {{'X', ' ', ' '},
+                          {' ', 'X', ' '},
+                          {' ', ' ', 'X'}};
         //Print the board
         System.out.println("-------");
         System.out.println("|" + board[0][0] + "|" + board[0][1] + "|" + board[0][2] + "|");
